0ccc180d1e35bfa5f36279655aa07a9831ce8a30
monishkaadlakha28 <monishkaadlakha24@gmail.com>
2025-12-12 23:59:09 +0530

    Add files via upload

---
+++ logical operator.sql
@@ -0,0 +1,9 @@
+-- Logical operator
+-- And () 
+use world;
+show tables;
+select* from country;
+select *from country where continent='asia' or region= 'middleeast';
+select *from country where continent='asia' or indepyear= '1901';
+select *from country where continent='asia' and indepyear= '1901';
+
